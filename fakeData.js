35279673554d2083023290a30fb661fca1b0fcfd
Sushanta Gupta <sushanta.gupta007@gmail.com>
2021-12-02 01:46:26 -0800

    Updates

---
+++ fakeData.js
@@ -0,0 +1,45 @@
+const services = [
+    {
+       id:001,      
+       name:'Online Consultation',
+       img: 'https://i.f1g.fr/media/eidos/640x432_crop/2020/06/08/XVMe9f34d3c-a962-11ea-968e-aabde546e914.jpg',
+       description:
+       'Fever, abdominal pain, eye infection, cold and cough are some of the issues that one could get treated by a General Physician. If you are experiencing any of these symptoms you must connect with a doctor at the earliest. You can use the online doctor consultation feature of United Health Group' 
+    },
+    {
+        id:002,      
+        name:'Diagnosis',
+        img: 'https://aealliance.org/wp-content/uploads/2021/02/Diagnosis.jpg',
+        description:
+        "United Health Group, Bangladesh's largest Diagnostic Centre & Pathology Lab offers a wide range of diagnostic tests & procedures across the country."
+     }, 
+     {
+        id:003,      
+        name:'Phychotherapy',
+        img: 'https://www.stepstorecovery.com/wp-content/uploads/2017/11/The-Psychotherapy-Definition.jpeg',
+        description:
+        "Psychotherapy is a general term for treating mental health problems by talking with a psychiatrist, psychologist or other mental health provider. During psychotherapy, you learn about your condition and your moods, feelings, thoughts and behaviors."
+     },
+     {
+        id:004,      
+        name:'Physiotheraphy',
+        img: 'https://www.stepstorecovery.com/wp-content/uploads/2017/11/The-Psychotherapy-Definition.jpeg',
+        description:
+        "Psychotherapy is a general term for treating mental health problems by talking with a psychiatrist, psychologist or other mental health provider. During psychotherapy, you learn about your condition and your moods, feelings, thoughts and behaviors."
+     }, 
+     {
+        id:005,      
+        name:'Maternity Facility',
+        img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRq-UITgFluQusl8bRMFm5nVGAMwo9jArP7vA&usqp=CAU',
+        description:
+        "A maternity hospital specializes in caring for women during pregnancy and childbirth. It also provides care for newborn infants, and may act as a centre for clinical training in midwifery and obstetrics."
+     }, 
+     {
+        id:006,      
+        name:'Cancer Care',
+        img: 'https://www.cancer.gov/sites/g/files/xnrzdm211/files/styles/cgov_social_media/public/cgov_image/media_image/100/500/8/files/woman-with-headscarf-getting-chemo-treatment-article.jpg',
+        description:
+        "CancerCare provides telephone, online and face-to-face counseling, support groups, education, publications and financial and co-payment assistance."
+     },
+]
+
---
+++ faketeam.js
@@ -0,0 +1,57 @@
+const services = [
+    {
+       id:001,      
+       name:'DR. Rebecca',
+       Designation: 'Consultant',
+       Degree: 'MBBS, MRCP (UK)',
+       img: 'https://img.freepik.com/free-photo/portrait-smiling-handsome-male-doctor-man_171337-5055.jpg?size=626&ext=jpg',
+       Biography:
+       'Obtained M.B.B.S from MAG Osmani Medical College, Sylhet; under Chittagong University in 1978. Completed internship in Sir Salimullah Medical College. Worked for seven years under Ministry of Health, Bangladesh.' 
+    },
+    {
+        id:002,      
+        name:'DR. Angelia',
+        Designation: 'Junior Consultant',
+        Degree: 'MBBS, MS (Cardiothoracic Surgery)',
+        img: 'https://img.freepik.com/free-photo/portrait-smiling-handsome-male-doctor-man_171337-5055.jpg?size=626&ext=jpg',
+        Biography:
+        'Obtained MBBS degree from Sher-E-Bangla Medical College, Barishal under University of Dhaka in 2004' 
+     },
+     {
+        id:003,      
+        name:'Dr. Diana',
+        Designation: 'Senior Consultant',
+        Degree: 'MBBS, DEM (Diploma in Endocrinology & Metabolism), MD (Endocrinology & Metabolism)',
+        img: 'https://img.freepik.com/free-photo/portrait-smiling-handsome-male-doctor-man_171337-5055.jpg?size=626&ext=jpg',
+        Biography:
+        'Obtained MBBS from Dhaka Medical College in 1984' 
+     },
+     {
+        id:004,      
+        name:'Dr. Jack Foster',
+        Designation: 'Senior Consultant',
+        Degree: 'MBBS, DDV (DU)',
+        img: 'https://img.freepik.com/free-photo/portrait-smiling-handsome-male-doctor-man_171337-5055.jpg?size=626&ext=jpg',
+        Biography:
+        'Obtained MBBS from Dhaka University in 1982' 
+     },
+     {
+        id:005,      
+        name:'Dr. Kalis Mickel',
+        Designation: 'Junior Consultant',
+        Degree: 'MBBS, FCPS (Hematology)',
+        img: 'https://img.freepik.com/free-photo/portrait-smiling-handsome-male-doctor-man_171337-5055.jpg?size=626&ext=jpg',
+        Biography:
+        'Obtained M.B.B.S from MAG Osmani Medical College, Sylhet; under Chittagong University in 1978. Completed internship in Sir Salimullah Medical College. Worked for seven years under Ministry of Health, Bangladesh.' 
+     },
+     {
+        id:006,      
+        name:'Dr. Mickel Kolins',
+        Designation: 'Senior Consultant',
+        Degree: 'MBBS (DMC), MSc (Canada), FCPS (Surgery), FRCS (Glasgow, UK)',
+        img: 'https://img.freepik.com/free-photo/portrait-smiling-handsome-male-doctor-man_171337-5055.jpg?size=626&ext=jpg',
+        Biography:
+        'Obtained M.B.B.S from MAG Osmani Medical College, Sylhet; under Chittagong University in 1978. Completed internship in Sir Salimullah Medical College. Worked for seven years under Ministry of Health, Bangladesh.' 
+     },
+]
+
